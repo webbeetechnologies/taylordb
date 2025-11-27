09479854c33ea4ad1f81e82f9832ead6ff289d54
Umar Adilov <99314948+adilovcode@users.noreply.github.com>
2025-11-27 16:40:33 +0500

    Fixed versioning

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,7 +8,7 @@ import { setTokenCommand } from './commands/set-token';
 
 const program = new Command();
 
-program.name('taylordb').description('CLI for TaylorDB').version('0.6.3');
+program.name('taylordb').description('CLI for TaylorDB').version('0.6.4');
 
 program.addCommand(loginCommand);
 program.addCommand(logoutCommand);
